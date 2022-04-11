0e723eb261b53ddf8e72e3b289967c1544cb1733
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2022-04-11 06:09:19 +0200

    Initial Commit

---
+++ ACalc/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ACalc.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
